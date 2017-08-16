736955288cc3a8dcd0662138cd58127f8b3de17c
Jonathan Chang <jonathanmchang@gmail.com>
2017-08-16 01:09:35 -0400

    TestFirst Part 01 Complete

---
+++ 01-Object-Properties/properties.js
@@ -0,0 +1,43 @@
+function setPropsOnObj(object) {
+    object.x = 7
+    object['y'] = 8
+    object.onePlus = function(num) {
+        return num + 1
+    }
+    return object
+}
+
+function setPropsOnArr(arrayObject) {
+    arrayObject.hello = function() {
+        return "Hello!"
+    }
+    arrayObject['full'] = 'stack'
+    arrayObject[0] = 5
+    arrayObject.twoTimes = function(num) {
+        return num * 2
+    }
+}
+
+function functionObject() {
+    
+}
+
+function setPropsOnFunc(obj) {
+    obj.year = '20??'
+    obj.divideByTwo = function(num) {
+        return num / 2
+    }
+    obj.prototype.helloWorld = function() {
+        return 'Hello World'     
+    }
+}
+
+// function setPropsOnFunc() {
+//     functionObject = function(){
+//     }
+//     functionObject.year = '20??'
+//     return functionObject
+//     functionObject.divideByTwo = function(num) {
+//         return num / 2
+//     }
+// }
